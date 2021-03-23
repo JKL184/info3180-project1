3151a611b2740a23c43abf50327b9736f9765680
Jordan Lewis <jordankareemlewis@gmail.com>
2021-03-23 02:59:31 -0500

    Real Estate

--- app/config.py
+++ app/config.py
@@ -2,11 +2,11 @@ import os
 
 class Config(object):
     """Base Config Object"""
-    DEBUG = False
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') 
+    DEBUG=False
+    SECRET_KEY= os.environ.get('SECRET_KEY') or 'Som3$ec5etK*y'
+    SQLALCHEMY_DATABASE_URI =os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS=False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
-    UPLOAD_FOLDER ='./uploads/'
+    UPLOAD_FOLDER='./uploads/'
     
 
 class DevelopmentConfig(Config):
