c8d45966728ff77e763b6c39342fd7485c5a3041
Jordan Lewis <jordankareemlewis@gmail.com>
2021-03-23 21:11:26 -0500

    increased model capacity

--- app/models.py
+++ app/models.py
@@ -4,20 +4,18 @@ from . import db
 
 class Property(db.Model):
     # You can use this to change the table name. The default convention is to use
-    # the class name. In this case a class name of UserProfile would create a
-    # user_profile (singular) table, but if we specify __tablename__ we can change it
-    # to `user_profiles` (plural) or some other name.
+    
     __tablename__ = 'properties'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80))
-    desc = db.Column(db.String(300))
-    room = db.Column(db.String(80))
-    bathroom = db.Column(db.String(80))
-    price = db.Column(db.String(80))
-    proptype= db.Column(db.String(80))
-    location = db.Column(db.String(80))
-    photo = db.Column(db.String(80))
+    title = db.Column(db.String(300))
+    desc = db.Column(db.String(1000))
+    room = db.Column(db.String(300))
+    bathroom = db.Column(db.String(300))
+    price = db.Column(db.String(300))
+    proptype= db.Column(db.String(300))
+    location = db.Column(db.String(300))
+    photo = db.Column(db.String(300))
 
     def __init__(self, title,desc,room,bathroom,price,proptype,location,photo):
         self.title=title
